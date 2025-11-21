0b8e77e13874e2ceac59890494356c047426bdda
Pietro Rhyan <pietroqjg@gmail.com>
2025-11-21 02:54:47 -0300

    feat: add create account to signin page

--- src/app/account/sign-in/page.tsx
+++ src/app/account/sign-in/page.tsx
@@ -13,69 +13,78 @@ export default function SignIn() {
 	const { isVisible, handleVisibility } = useInputVisibility()
 
 	return (
-		<Card title='Fazer login' width={480}>
-			<div className='w-full flex flex-col gap-6'>
-				<p>
-					Seja bem-vindo ao SpaceChat! Por favor, insira suas credenciais para
-					acessar sua conta
-				</p>
+		<div className='flex flex-col gap-2' >
+			<Card title='Fazer login' width={480}>
+				<div className='w-full flex flex-col gap-6'>
+					<p>
+						Seja bem-vindo ao SpaceChat! Por favor, insira suas credenciais para
+						acessar sua conta
+					</p>
 
-				<form action='' className='w-full flex flex-col gap-6'>
-					<div className='w-full flex flex-col gap-2'>
-						<Label htmlFor='email'>E-mail</Label>
-						<Input
-							id='email'
-							type='email'
-							minLength={6}
-							placeholder='Digite seu e-mail'
-							autoComplete='off'
-						/>
-					</div>
-
-					<div className='w-full flex flex-col gap-2'>
-						<Label htmlFor='password'>Senha</Label>
-						<Input
-							id='password'
-							type={isVisible ? 'text' : 'password'}
-							minLength={6}
-							placeholder='Digite sua senha'
-							autoComplete='off'
-							icon={
-								<VisibleInputIcon
-									isVisible={isVisible}
-									handleVisibility={handleVisibility}
-								/>
-							}
-						/>
-
-						<Link
-							href='#'
-							className='text-primary underline underline-offset-2 ml-auto'
-						>
-							Esqueci minha senha
-						</Link>
-					</div>
-
-					<div className='w-full mt-3 flex flex-col gap-6'>
-						<Button title='Realizar login' type='submit' fullWidth />
-
-						<div className='relative w-full flex items-center justify-center'>
-							<div className='h-px w-full bg-neutral-900/20' />
-							<p className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-black-900 text-neutral-900 px-1'>
-								ou
-							</p>
+					<form action='' className='w-full flex flex-col gap-6'>
+						<div className='w-full flex flex-col gap-2'>
+							<Label htmlFor='email'>E-mail</Label>
+							<Input
+								id='email'
+								type='email'
+								minLength={6}
+								placeholder='Digite seu e-mail'
+								autoComplete='off'
+							/>
 						</div>
 
-						<Button
-							title='Acessar com Google'
-							buttonType='neutral'
-							leftIcon={<FcGoogle size={24} />}
-							type='button'
-							fullWidth
-						/>
-					</div>
-				</form>
-			</div>
-		</Card>
+						<div className='w-full flex flex-col gap-2'>
+							<Label htmlFor='password'>Senha</Label>
+							<Input
+								id='password'
+								type={isVisible ? 'text' : 'password'}
+								minLength={6}
+								placeholder='Digite sua senha'
+								autoComplete='off'
+								icon={
+									<VisibleInputIcon
+										isVisible={isVisible}
+										handleVisibility={handleVisibility}
+									/>
+								}
+							/>
+
+							<Link
+								href='#'
+								className='text-primary underline underline-offset-2 ml-auto'
+							>
+								Esqueci minha senha
+							</Link>
+						</div>
+
+						<div className='w-full mt-3 flex flex-col gap-6'>
+							<Button title='Realizar login' type='submit' fullWidth />
+
+							<div className='relative w-full flex items-center justify-center'>
+								<div className='h-px w-full bg-neutral-900/20' />
+								<p className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-black-900 text-neutral-900 px-1'>
+									ou
+								</p>
+							</div>
+
+							<Button
+								title='Acessar com Google'
+								buttonType='neutral'
+								leftIcon={<FcGoogle size={24} />}
+								type='button'
+								fullWidth
+							/>
+						</div>
+					</form>
+				</div>
+			</Card>
+
+			<Link
+				href='#'
+				className='text-primary underline underline-offset-2'
+			>
+				Não tem uma conta? Criar conta
+			</Link>
+		</div>
 	)
 }
