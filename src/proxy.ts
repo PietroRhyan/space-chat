2a20c933d99810ad8b4aae9446fbdba9083da8eb
Pietro Rhyan <pietroqjg@gmail.com>
2025-12-04 19:10:24 -0300

    feat: add proxy to handle protected routes

---
+++ src/proxy.ts
@@ -0,0 +1,43 @@
+import { type NextRequest, NextResponse } from 'next/server'
+
+const publicRoutes = ['/landing']
+const authRoutes = ['/account/sign-in', '/account/sign-on']
+
+export function proxy(req: NextRequest) {
+  const { nextUrl } = req
+
+  const isAuthenticated = !!req.cookies.get('better-auth.session_token')?.value
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if (isAuthRoute) {
+    if (isAuthenticated) {
+      return NextResponse.redirect(new URL('/', nextUrl))
+    }
+
+    return NextResponse.next()
+  }
+
+  if (!isAuthenticated && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname
+
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+
+    return NextResponse.redirect(
+      new URL(`/sign-in?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    )
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+}
